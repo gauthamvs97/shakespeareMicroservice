09dacd1a4bdba715c6cc7049126a3f182f7fa17d
gauthamvs97 <34578277+gauthamvs97@users.noreply.github.com>
2021-07-25 19:49:35 -0500

    Update analyse_Shake.py

--- shakespeare/python_Programs/analyse_Shake.py
+++ shakespeare/python_Programs/analyse_Shake.py
@@ -1,7 +1,7 @@
 import json
 import time
 
-def clean_Word(word):  # function that takes a word and returns only the alphabets in it. Ex: day's = days
+def clean_Word(word):  # function that takes a word and returns only the alphabets in it. Ex: day's -> days, python3 -> python
     only_alpha = ""
     for char in word:
         if ((ord(char) >= 65 and ord(char) <= 90) or ((ord(char) >= 97 and ord(char) <= 122))):
@@ -9,11 +9,11 @@ def clean_Word(word):  # function that takes a word and returns only the alphabe
 
     return only_alpha
 
-def count_Shakespeare(inputFile):
+def count_Shakespeare(inputFile):                   #function that is called in views.py
 
-    start_Time = time.time()
+    start_Time = time.time()                        #start time is stored
     n = 0
-    with open(inputFile) as f:
+    with open(inputFile) as f:                      #input file is read and stores in a list, line by line
         lines = [line.strip() for line in f]
 
     total_Length = len(lines)
@@ -23,13 +23,13 @@ def count_Shakespeare(inputFile):
     processingResult = list()
 
     cur = 0
-    for i in range(cur, total_Length - 1):
-        if (i > 35 and len(lines[i]) > 0 and not lines[i] in chapters):
+    for i in range(cur, total_Length - 1):                               #main content of the shakespeare file starts from line 36
+        if (i > 35 and len(lines[i]) > 0 and not lines[i] in chapters):   #extracting and storing each chapter name from index
             chapters.append(lines[i])
         elif (lines[i] in chapters):
             break
         cur = cur + 1
-
+                                                            #starting with the main processing
     for i in range(cur, total_Length - 1):
         if lines[i] in chapters:
             chapterName = lines[i]
@@ -42,19 +42,19 @@ def count_Shakespeare(inputFile):
                                 'w': 0, 'x': 0, 'y': 0, 'z': 0}
             unique_Words = []
             for j in range(i + 1, total_Length - 1):
-                if (len(lines[j]) < 2):
+                if (len(lines[j]) < 2):                     #length of line
                     j = j + 1
-                elif (lines[j] in chapters):
+                elif (lines[j] in chapters):                #next chapter
                     break
                 else:
                     c_Lines += 1
                     cur_line = lines[j].split()
                     for cur_word in cur_line:
-                        cur_word = clean_Word(cur_word)  # cleaned word with only alphabets
-                        if (len(cur_word) < 1):  # if word after cleaning is null,
+                        cur_word = clean_Word(cur_word)     # cleaned word with only alphabets
+                        if (len(cur_word) < 1):             # if word after cleaning is null,
                             continue
 
-                        c = cur_word[0]  # for finding words that begin with same characters
+                        c = cur_word[0]                     # for finding words that begin with same characters
                         word_with_Letter[c] += 1
 
                         if (cur_word not in unique_Words):  # finding unique words and counting
@@ -62,21 +62,16 @@ def count_Shakespeare(inputFile):
                             c_UniqueWords += 1
                 j += 1
 
-            chapInfo["chapterName "] = chapterName
+            chapInfo["chapterName "] = chapterName                  #storing info for each entry
             chapInfo["noOfLines"] = c_Lines
             chapInfo["noOfUniqueWords"] = c_UniqueWords
             chapInfo["wordsByCharacter"] = word_with_Letter
 
-            processingResult.append(chapInfo)
-
-            # print("Unique words is ", c_UniqueWords)
-            # print("Number of lines is ", c_Lines)
-            # print(word_with_Letter, "\n")
-            i = j
+            processingResult.append(chapInfo)                       #each entry is added to a list
 
     end_Time = time.time()
     processingTime = end_Time - start_Time
-    # print(processingTime)
+    print(processingTime)
 
     response["finalResults"] = processingResult
     response["totalTime"] = processingTime
